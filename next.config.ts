0d4df6cc9b267130df89dd1efae0b37f0443f5c1
Jonathandelasen <jonathandelasen@gmail.com>
2026-06-06 17:40:17 +0200

    feat: add 192.168.1.130 to allowed development origins in next configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.130"],
   serverExternalPackages: ["@napi-rs/canvas", "pdf-parse"],
   outputFileTracingIncludes: {
     "/api/**/*": [
