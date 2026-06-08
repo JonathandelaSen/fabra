fd370bf1dc1c3b1596202cd177b14621abf55ca9
Jonathandelasen <jonathandelasen@gmail.com>
2026-06-08 18:53:37 +0200

    refactor: remove unused CV analysis detail view component and associated normalizer utility

--- src/features/cv-analysis/index.ts
+++ src/features/cv-analysis/index.ts
@@ -1,6 +1,1 @@
 export { default as CVAnalysisView } from "./components/cv-analysis-view";
-export {
-  default as CVAnalysisDetailView,
-  type CVAnalysisDetail,
-  type CVAnalysisDetailTab,
-} from "./components/cv-analysis-detail-view";
